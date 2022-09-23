9a2deb4be5396507a593185da021e794dbe77a34
月城蔵徒 <104145991+Kurato-Tsukishiro@users.noreply.github.com>
2022-09-24 06:27:45 +0900

    BANのコードを追加
    よっキングさん案に一部追加

--- SuperSimplePlus/Patches/NotPCKick.cs
+++ SuperSimplePlus/Patches/NotPCKick.cs
@@ -7,20 +7,21 @@ namespace SuperSimplePlus.Patches
     public class NotPCKick
     {
         public static bool IsKick = false;
+        public static bool IsBan = false;
     }
     [HarmonyPatch(typeof(GameStartManager), nameof(GameStartManager.Update))]
     public class GameStartManagerUpdatePatch
     {
         public static void Postfix(GameStartManager __instance)
         {
-            if (AmongUsClient.Instance.AmHost && NotPCKick.IsKick)
+            if (AmongUsClient.Instance.AmHost && (NotPCKick.IsKick || NotPCKick.IsBan))
             {
                 foreach (InnerNet.ClientData p in AmongUsClient.Instance.allClients)
                 {
                     if (p.PlatformData.Platform is not Platforms.StandaloneEpicPC and not Platforms.StandaloneSteamPC)
                     {
-                        AmongUsClient.Instance.KickPlayer(p.Id, false);
-                        Logger.Info($"Kick PlayerName:{p.PlayerName}({p.PlatformData.Platform})", "NotPCKick");
+                        AmongUsClient.Instance.KickPlayer(p.Id, NotPCKick.IsBan);
+                        Logger.Info($"{(NotPCKick.IsBan ? "BAN" : "Kick")} PlayerName:{p.PlayerName}({p.PlatformData.Platform})", $"NotPC{(NotPCKick.IsBan ? "BAN" : "Kick")}");
                     }
                 }
             }
