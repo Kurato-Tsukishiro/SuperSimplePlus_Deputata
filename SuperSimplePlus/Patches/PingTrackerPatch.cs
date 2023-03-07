f9714428c614ac616b24ee475b2563c7bafb6ece
月城蔵徒 <104145991+Kurato-Tsukishiro@users.noreply.github.com>
2023-03-07 16:28:34 +0900

    namespace ○○;形式に変更

--- SuperSimplePlus/Patches/PingTrackerPatch.cs
+++ SuperSimplePlus/Patches/PingTrackerPatch.cs
@@ -1,19 +1,17 @@
 using HarmonyLib;
 using UnityEngine;
 
-namespace SuperSimplePlus.Patches
+namespace SuperSimplePlus.Patches;
+public class PingTrackerPatch
 {
-    public class PingTrackerPatch
+    [HarmonyPatch(typeof(PingTracker), nameof(PingTracker.Update))]
+    public class PingTrackerUpdatePatch
     {
-        [HarmonyPatch(typeof(PingTracker), nameof(PingTracker.Update))]
-        public class PingTrackerUpdatePatch
+        public static void Postfix(PingTracker __instance)
         {
-            public static void Postfix(PingTracker __instance)
-            {
-                __instance.text.alignment = TMPro.TextAlignmentOptions.TopRight;
-                __instance.text.text = ThisAssembly.Git.Branch == "main" ? $"{SSPPlugin.ColoredModName} ver.{SSPPlugin.Version}\n{__instance.text.text}" : $"{SSPPlugin.ColoredModName} ver.{SSPPlugin.Version}\n{ThisAssembly.Git.Branch}({ThisAssembly.Git.Commit})\n{__instance.text.text}";
-                __instance.gameObject.GetComponent<AspectPosition>().DistanceFromEdge = new Vector3(1.2f, 0.1f, 0.5f);
-            }
+            __instance.text.alignment = TMPro.TextAlignmentOptions.TopRight;
+            __instance.text.text = ThisAssembly.Git.Branch == "main" ? $"{SSPPlugin.ColoredModName} ver.{SSPPlugin.Version}\n{__instance.text.text}" : $"{SSPPlugin.ColoredModName} ver.{SSPPlugin.Version}\n{ThisAssembly.Git.Branch}({ThisAssembly.Git.Commit})\n{__instance.text.text}";
+            __instance.gameObject.GetComponent<AspectPosition>().DistanceFromEdge = new Vector3(1.2f, 0.1f, 0.5f);
         }
     }
 }
