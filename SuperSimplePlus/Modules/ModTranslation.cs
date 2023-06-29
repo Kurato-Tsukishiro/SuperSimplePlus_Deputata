9842d8650f0ba3b72473f2f3d450a2bb3a8e4524
Kurato-Tsukishiro <kurato.snr7@gmail.com>
2023-06-29 09:54:57 +0900

    翻訳をExcelからcsvに変更
    # 参考 => https://github.com/ykundesu/SuperNewRoles/blob/1.8.1.0/SuperNewRoles/Resources/Translate.csv

--- SuperSimplePlus/Modules/ModTranslation.cs
+++ SuperSimplePlus/Modules/ModTranslation.cs
@@ -1,90 +1,63 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
-using System.Text.RegularExpressions;
 using AmongUs.Data;
-using Newtonsoft.Json.Linq;
 
 namespace SuperSimplePlus;
-public class ModTranslation
+public static class ModTranslation
 {
-    public static int defaultLanguage = (int)SupportedLangs.English;
-    public static Dictionary<string, Dictionary<int, string>> stringData;
-
-    private const string blankText = "[BLANK]";
-
-    public ModTranslation()
+    // 一番左と一行全部
+    private static Dictionary<string, string[]> dictionary = new();
+    public static string GetString(string key)
     {
+        // アモアス側の言語読み込みが完了しているか ? 今の言語 : 最後の言語
+        SupportedLangs langId = TranslationController.InstanceExists ? TranslationController.Instance.currentLanguage.languageID : DataManager.Settings.Language.CurrentLanguage;
 
+        if (!dictionary.ContainsKey(key)) return key; // keyが辞書にないならkeyのまま返す
+
+        return langId switch
+        {
+            SupportedLangs.English => dictionary[key][1], // 英語
+            SupportedLangs.Japanese => dictionary[key][2],// 日本語
+            _ => dictionary[key][1] // それ以外は英語
+        };
     }
 
-
-    public static void Load()
+    public static void LoadCsv()
     {
-        Assembly assembly = Assembly.GetExecutingAssembly();
-        Stream stream = assembly.GetManifestResourceStream("SuperSimplePlus.Resources.TranslationData.json");
-        var byteArray = new byte[stream.Length];
-        var read = stream.Read(byteArray, 0, (int)stream.Length);
-        string json = System.Text.Encoding.UTF8.GetString(byteArray);
+        var fileName = Assembly.GetExecutingAssembly().GetManifestResourceStream("SuperSimplePlus.Resources.Translate.csv");
 
-        stringData = new Dictionary<string, Dictionary<int, string>>();
-        JObject parsed = JObject.Parse(json);
+        //csvを開く
+        StreamReader sr = new(fileName);
 
-        for (int i = 0; i < parsed.Count; i++)
+        var i = 0;
+        //1行ずつ処理
+        while (!sr.EndOfStream)
         {
-            JProperty token = parsed.ChildrenTokens[i].TryCast<JProperty>();
-            if (token == null) continue;
-
-            string stringName = token.Name;
-            var val = token.Value.TryCast<JObject>();
-
-            if (token.HasValues)
+            try
             {
-                var strings = new Dictionary<int, string>();
+                // 行ごとの文字列
+                string line = sr.ReadLine();
 
-                for (int j = 0; j < (int)SupportedLangs.Irish + 1; j++)
+                // 行が空白 戦闘が*なら次の行に
+                if (line == "" || line[0] == '#') continue;
+
+                //カンマで配列の要素として分ける
+                string[] values = line.Split(',');
+
+                // 配列から辞書に格納する
+                List<string> valuesList = new();
+                foreach (string vl in values)
                 {
-                    string key = j.ToString();
-                    var text = val[key]?.TryCast<JValue>().Value.ToString();
-
-                    if (text != null && text.Length > 0)
-                    {
-                        strings[j] = text == blankText ? "" : text;
-                    }
+                    valuesList.Add(vl.Replace("\\n", "\n").Replace("，", ","));
                 }
-
-                stringData[stringName] = strings;
+                dictionary.Add(values[0], valuesList.ToArray());
+                i++;
+            }
+            catch
+            {
+                Logger.Error($"Error: Loading Translate.csv Line:{i}", "ModTranslation");
             }
         }
-
-        Logger.Info($"Language: {stringData.Keys}", "ModTranslation");
-    }
-
-    public static string GetString(string key, string def = null)
-    {
-        // Strip out color tags.
-        string keyClean = Regex.Replace(key, "<.*?>", "");
-        keyClean = Regex.Replace(keyClean, "^-\\s*", "");
-        keyClean = keyClean.Trim();
-
-        def ??= key;
-        if (!stringData.ContainsKey(keyClean))
-        {
-            return def;
-        }
-
-        var data = stringData[keyClean];
-        int lang = (int)DataManager.Settings.Language.CurrentLanguage;
-
-        if (data.ContainsKey(lang))
-        {
-            return key.Replace(keyClean, data[lang]);
-        }
-        else if (data.ContainsKey(defaultLanguage))
-        {
-            return key.Replace(keyClean, data[defaultLanguage]);
-        }
-
-        return key;
     }
 }
