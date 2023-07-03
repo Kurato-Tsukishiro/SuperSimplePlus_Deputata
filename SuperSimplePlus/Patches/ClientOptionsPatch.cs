8bc7c4004c3abfa31da5ddace1bc118aa8f7ddab
Kurato-Tsukishiro <kurato.snr7@gmail.com>
2023-07-03 13:50:14 +0900

    SSPの設定を設定画面上に移動した

--- SuperSimplePlus/Patches/ClientOptionsPatch.cs
+++ SuperSimplePlus/Patches/ClientOptionsPatch.cs
@@ -1,4 +1,5 @@
-//参考=>https://github.com/Eisbison/TheOtherRoles/blob/main/TheOtherRoles/Patches/ClientOptionsPatch.cs
+//参考 => https://github.com/Eisbison/TheOtherRoles/blob/main/TheOtherRoles/Patches/ClientOptionsPatch.cs
+//参考 => https://github.com/ykundesu/SuperNewRoles/blob/1.8.1.0/SuperNewRoles/Patches/ClientOptionsPatch/ModOptions.cs
 using System;
 using System.Collections.Generic;
 using HarmonyLib;
@@ -9,29 +10,30 @@ using static UnityEngine.UI.Button;
 using Object = UnityEngine.Object;
 
 namespace SuperSimplePlus.Patches;
+
 [HarmonyPatch]
-public static class ClientOptionsPatch
+internal static class ClientOptionsPatch
 {
-    private static readonly SelectionBehaviour[] AllOptions =
-        {
+    private static readonly SelectionBehaviour[] AllOptions = {
             new(ModTranslation.GetString("NotPCKick"),()=> SSPPlugin.NotPCKick.Value = !SSPPlugin.NotPCKick.Value,SSPPlugin.NotPCKick.Value),
             new(ModTranslation.GetString("NotPCBan"),()=> SSPPlugin.NotPCBan.Value = !SSPPlugin.NotPCBan.Value,SSPPlugin.NotPCBan.Value),
+            new(ModTranslation.GetString("FriendCodeBan"),()=> SSPPlugin.FriendCodeBan.Value = !SSPPlugin.FriendCodeBan.Value,SSPPlugin.FriendCodeBan.Value),
             new(ModTranslation.GetString("ChatLog"),()=> SSPPlugin.ChatLog.Value = !SSPPlugin.ChatLog.Value,SSPPlugin.ChatLog.Value),
             new(ModTranslation.GetString("HideFriendCode"),()=> SSPPlugin.HideFriendCode.Value = !SSPPlugin.HideFriendCode.Value,SSPPlugin.HideFriendCode.Value),
-            new(ModTranslation.GetString("FriendCodeBan"),()=> SSPPlugin.FriendCodeBan.Value = !SSPPlugin.FriendCodeBan.Value,SSPPlugin.FriendCodeBan.Value),
-        };
+    };
 
-    public static PassiveButton SSPSettingButton;
-    public static SpriteRenderer SSPSettingSpriteRenderer;
-    public static GameObject SSPOptionsMenu;
+    private static GameObject popUp;
     private static TextMeshPro titleText;
+
+    private static ToggleButtonBehaviour moreOptions;
+    private static List<ToggleButtonBehaviour> modButtons;
+    private static TextMeshPro titleTextTitle;
 
     private static ToggleButtonBehaviour buttonPrefab;
 
-
     [HarmonyPostfix]
     [HarmonyPatch(typeof(MainMenuManager), nameof(MainMenuManager.Start))]
-    public static void MainMenuManager_StartPostfix(MainMenuManager __instance)
+    internal static void MainMenuManager_StartPostfix(MainMenuManager __instance)
     {
         // Prefab for the title
         var go = new GameObject("TitleTextSSP");
@@ -44,64 +46,93 @@ public static class ClientOptionsPatch
     }
 
     [HarmonyPostfix]
+    [HarmonyPatch(typeof(OptionsMenuBehaviour), nameof(OptionsMenuBehaviour.Start))]
+    internal static void OptionsMenuBehaviour_StartPostfix(OptionsMenuBehaviour __instance)
+    {
+        if (!__instance.CensorChatButton) return;
+
+        if (!popUp)
+        {
+            CreateCustom(__instance);
+        }
+
+        SetUpOptions();
+        InitializeMoreButton(__instance);
+    }
+
+    private static void CreateCustom(OptionsMenuBehaviour prefab)
+    {
+        popUp = Object.Instantiate(prefab.gameObject);
+        Object.DontDestroyOnLoad(popUp);
+        var transform = popUp.transform;
+        var pos = transform.localPosition;
+        pos.z = -810f;
+        transform.localPosition = pos;
+
+        Object.Destroy(popUp.GetComponent<OptionsMenuBehaviour>());
+        foreach (var gObj in popUp.gameObject.GetAllChilds())
+        {
+            if (gObj.name is not "Background" and not "CloseButton")
+                Object.Destroy(gObj);
+        }
+        popUp.SetActive(false);
+    }
+    [HarmonyPostfix]
     [HarmonyPatch(typeof(HudManager), nameof(HudManager.Start))]
     public static void HudManager_StartPostfix()
     {
-        SSPSettingButton = GameObject.Instantiate(FastDestroyableSingleton<HudManager>.Instance.MapButton, HudManager.Instance.transform.FindChild("Buttons").FindChild("TopRight"));
-
-        SSPSettingSpriteRenderer = SSPSettingButton.GetComponent<SpriteRenderer>();
-
-        SSPSettingSpriteRenderer.sprite = Helpers.loadSpriteFromResources("SuperSimplePlus.Resources.SettingButton.png", 115f);
-
-        SSPSettingSpriteRenderer.gameObject.SetActive(true);
-        SSPSettingSpriteRenderer.enabled = true;
-
-        SSPSettingButton.OnClick = new ButtonClickedEvent();
-
-        SSPSettingButton.OnClick.AddListener((UnityAction)(() => { SSPSettingButtonOnClick(); }));
-
         buttonPrefab = Object.Instantiate(FastDestroyableSingleton<HudManager>.Instance.transform.FindChild("Menu").GetComponent<OptionsMenuBehaviour>().CensorChatButton);
         Object.DontDestroyOnLoad(buttonPrefab);
         buttonPrefab.name = "CensorChatPrefab";
         buttonPrefab.gameObject.SetActive(false);
-
-        SSPSettingSpriteRenderer.gameObject.transform.localPosition = new(SSPSettingButton.transform.localPosition.x, SSPSettingButton.transform.localPosition.y - 0.75f, SSPSettingButton.transform.localPosition.z);
     }
 
-    private static void SSPSettingButtonOnClick()
+    private static void InitializeMoreButton(OptionsMenuBehaviour __instance)
     {
-        if (SSPOptionsMenu) return;
-
-        var __instance = FastDestroyableSingleton<HudManager>.Instance;
-
-        SSPOptionsMenu = Object.Instantiate(__instance.transform.FindChild("Menu").GetComponent<OptionsMenuBehaviour>().Background.gameObject);
-        SSPOptionsMenu.transform.SetParent(__instance.transform);
-        SSPOptionsMenu.transform.position = __instance.transform.FindChild("Menu").GetComponent<OptionsMenuBehaviour>().transform.position;
-        SSPOptionsMenu.transform.localScale = new(SSPOptionsMenu.transform.localScale.x * 0.9f, SSPOptionsMenu.transform.localScale.y * 0.9f, 0f);
-
-        PassiveButton closeButton = GameObject.Instantiate(SSPSettingButton, SSPSettingButton.transform);
-
-        SpriteRenderer closeSpriteRenderer = closeButton.GetComponent<SpriteRenderer>();
-        closeSpriteRenderer.gameObject.transform.SetParent(SSPOptionsMenu.transform);
-        closeSpriteRenderer.gameObject.transform.localPosition = new(2.25f, 2.44f, SSPOptionsMenu.transform.localPosition.z);
-        closeSpriteRenderer.gameObject.layer = 5;
-        closeSpriteRenderer.sortingOrder = 1;
-
-        closeSpriteRenderer.sprite = Helpers.loadSpriteFromResources("SuperSimplePlus.Resources.CloseButton.png", 115f);
-
-        closeSpriteRenderer.gameObject.SetActive(true);
-        closeSpriteRenderer.enabled = true;
-
-        closeButton.OnClick = new ButtonClickedEvent();
-
-        closeButton.OnClick.AddListener((UnityAction)(() =>
+        moreOptions = Object.Instantiate(buttonPrefab, __instance.CensorChatButton.transform.parent);
+        Vector3 originPosition = new(-1.4f, -0.5f, -1.0f);
+        moreOptions.transform.localPosition = originPosition + new Vector3(0.0f, -0.5f, 0.0f);
+        var trans = moreOptions.transform.localPosition;
+        moreOptions.gameObject.SetActive(true);
+        trans = moreOptions.transform.position;
+        var buttonSpriteRenderer = moreOptions.gameObject.AddComponent<SpriteRenderer>();
+        buttonSpriteRenderer.sprite = Helpers.loadSpriteFromResources("SuperSimplePlus.Resources.SettingButton.png", 115f);
+        buttonSpriteRenderer.size = new Vector2(1.0f, 1.0f);
+        moreOptions.Text.text = ModTranslation.GetString("<color=#FFFFFF00>modOptionsText</color>");
+        var moreOptionsButton = moreOptions.GetComponent<PassiveButton>();
+        moreOptions.Background.color = new Color(255f, 255f, 255f, 0);
+        moreOptionsButton.OnClick = new ButtonClickedEvent();
+        moreOptionsButton.OnClick.AddListener((Action)(() =>
         {
-            GameObject.Destroy(SSPOptionsMenu);
+            if (!popUp) return;
+            if (__instance.transform.parent && __instance.transform.parent == FastDestroyableSingleton<HudManager>.Instance.transform)
+            {
+                popUp.transform.SetParent(FastDestroyableSingleton<HudManager>.Instance.transform);
+                popUp.transform.localPosition = new Vector3(0, 0, -800f);
+            }
+            else
+            {
+                popUp.transform.SetParent(null);
+                Object.DontDestroyOnLoad(popUp);
+            }
+            CheckSetTitle();
+            RefreshOpen();
         }));
+    }
 
-        var title = Object.Instantiate(titleText, SSPOptionsMenu.transform);
-        title.GetComponent<RectTransform>().localPosition = Vector3.up * 2.0f;
-        title.gameObject.layer = 5;
+    private static void RefreshOpen()
+    {
+        popUp.gameObject.SetActive(false);
+        popUp.gameObject.SetActive(true);
+        SetUpOptions();
+    }
+
+    private static void CheckSetTitle()
+    {
+        if (!popUp || popUp.GetComponentInChildren<TextMeshPro>() || !titleText) return;
+
+        var title = titleTextTitle = Object.Instantiate(titleText, popUp.transform);
+        title.GetComponent<RectTransform>().localPosition = Vector3.up * 2.3f;
         title.gameObject.SetActive(true);
         title.text = ModTranslation.GetString("SSPSettings");
         title.name = "TitleText";
@@ -111,12 +142,15 @@ public static class ClientOptionsPatch
 
     private static void SetUpOptions()
     {
+        if (popUp.transform.GetComponentInChildren<ToggleButtonBehaviour>()) return;
+
+        modButtons = new List<ToggleButtonBehaviour>();
+
         for (var i = 0; i < AllOptions.Length; i++)
         {
             var info = AllOptions[i];
 
-            var button = Object.Instantiate(buttonPrefab, SSPOptionsMenu.transform);
-
+            var button = Object.Instantiate(buttonPrefab, popUp.transform);
             var pos = new Vector3(i % 2 == 0 ? -1.17f : 1.17f, 1.3f - i / 2 * 0.8f, -.5f);
 
             var transform = button.transform;
@@ -126,7 +160,7 @@ public static class ClientOptionsPatch
             button.Background.color = button.onState ? Color.green : Palette.ImpostorRed;
 
             button.Text.text = ModTranslation.GetString(info.Title);
-            button.Text.fontSizeMin = button.Text.fontSizeMax = 2.5f;
+            button.Text.fontSizeMin = button.Text.fontSizeMax = 2.2f;
             button.Text.font = Object.Instantiate(titleText.font);
             button.Text.GetComponent<RectTransform>().sizeDelta = new Vector2(2, 2);
 
@@ -153,9 +187,9 @@ public static class ClientOptionsPatch
 
             foreach (var spr in button.gameObject.GetComponentsInChildren<SpriteRenderer>())
                 spr.size = new Vector2(2.2f, .7f);
+            modButtons.Add(button);
         }
     }
-
     private static IEnumerable<GameObject> GetAllChilds(this GameObject Go)
     {
         for (var i = 0; i < Go.transform.childCount; i++)
@@ -164,13 +198,13 @@ public static class ClientOptionsPatch
         }
     }
 
-    public class SelectionBehaviour
+    internal class SelectionBehaviour
     {
-        public string Title;
-        public Func<bool> OnClick;
-        public bool DefaultValue;
+        internal string Title;
+        internal Func<bool> OnClick;
+        internal bool DefaultValue;
 
-        public SelectionBehaviour(string title, Func<bool> onClick, bool defaultValue)
+        internal SelectionBehaviour(string title, Func<bool> onClick, bool defaultValue)
         {
             Title = title;
             OnClick = onClick;
