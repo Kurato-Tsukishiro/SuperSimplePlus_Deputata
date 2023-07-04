3bfc9a25ccb517978966310bc1510ebfed1ef853
Kurato-Tsukishiro <kurato.snr7@gmail.com>
2023-07-04 17:40:10 +0900

    2.1の場合ブランチ名の色を変更するコードを作成

--- SuperSimplePlus/Patches/PingTrackerPatch.cs
+++ SuperSimplePlus/Patches/PingTrackerPatch.cs
@@ -13,6 +13,8 @@ public class PingTrackerPatch
             __instance.text.text =
                 ThisAssembly.Git.Branch == "main" ?
                     $"{SSPPlugin.ColoredModName} ver.{SSPPlugin.Version}\n{__instance.text.text}" :
+                ThisAssembly.Git.Branch == "master_standard2.1" ?
+                    $"{SSPPlugin.ColoredModName} ver.{SSPPlugin.Version}\n<color=#6e7955>{bc}</color>\n{__instance.text.text}" :
                     $"{SSPPlugin.ColoredModName} ver.{SSPPlugin.Version}\n{bc}\n{__instance.text.text}";
             __instance.gameObject.GetComponent<AspectPosition>().DistanceFromEdge = new Vector3(1.2f, 0.1f, 0.5f);
         }
