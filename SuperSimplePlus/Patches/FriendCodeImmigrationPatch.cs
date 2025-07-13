3507ab58a09f6a1f260ad7fc16ee78f0ea41cf64
Kurato-Tsukishiro <kurato.snr7@gmail.com>
2025-07-13 13:53:31 +0900

    fix: 既入室者の警告メッセージをリファクタした
    - 参考 => https://g.co/gemini/share/a44da4e0b41a
    - foreachでなくLinkを使用するようにした
    - 警告メッセージを表示するか否かで文章の組み立てを分岐せず変数を使用して組み立てるようにした

--- SuperSimplePlus/Patches/FriendCodeImmigrationPatch.cs
+++ SuperSimplePlus/Patches/FriendCodeImmigrationPatch.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using InnerNet;
 
 namespace SuperSimplePlus.Patches;
@@ -26,12 +27,7 @@ internal static class FriendCodeImmigrationPatch
                 }
             }
 
-            string? warningMessage = null;
-            foreach (KeyValuePair<int, string> kvp in warningTextDic) { warningMessage += $"{kvp.Value}\n"; }
-            if (warningMessage == null)
-                __instance.AddChat(PlayerControl.LocalPlayer, $"<align={"left"}><color=#89c3eb><size=150%>Infomation</size></color><size=80%>\n現在, BANList対象者は入室しておりません。</size></align>");
-            else
-                __instance.AddChat(PlayerControl.LocalPlayer, $"<align={"left"}><color=#F2E700><size=150%>警告!</size></color><size=80%>\n{warningMessage}</size></align>");
+            __instance.AddChat(PlayerControl.LocalPlayer, BuildPlayerWarningMessage(in warningTextDic));
         }
     }
 
@@ -96,21 +92,48 @@ internal static class FriendCodeImmigrationPatch
 
         // FIXME : 入室タイミングでのチャット表示は ``FastDestroyableSingleton<HudManager>.Instance.Chat`` が null の為使用できない。タイマーとかで実行タイミングずらして表示できるようにする
         /*
-        string? warningMessage = null;
         if (FastDestroyableSingleton<HudManager>.Instance != null && FastDestroyableSingleton<HudManager>.Instance.Chat != null)
-        {
-            foreach (KeyValuePair<int, string> kvp in warningTextDic) { warningMessage += $"{kvp.Value}\n"; }
-
-            if (warningMessage == null)
-                FastDestroyableSingleton<HudManager>.Instance.Chat.AddChat(PlayerControl.LocalPlayer, $"<align={"left"}><color=#89c3eb><size=150%>Infomation</size></color><size=80%>\n現在, BANList対象者は入室しておりません。</size></align>");
-            else
-                FastDestroyableSingleton<HudManager>.Instance.Chat.AddChat(PlayerControl.LocalPlayer, $"<align={"left"}><color=#F2E700><size=150%>警告!</size></color><size=80%>\n{warningMessage}</size></align>");
-        }
+            FastDestroyableSingleton<HudManager>.Instance.Chat.AddChat(PlayerControl.LocalPlayer, CreatePlayerWarningMessage(in warningTextDic));
         */
+
 
         Logger.Info($"|:========== 既入室者の記録 Start ==========:|", "AmongUsClientOnPlayerJoindPatch");
         foreach (KeyValuePair<int, string> kvp in participantDic) Logger.Info(kvp.Value, "OnPlayerJoined");
 
         Logger.Info($"|:========== 既入室者の記録 End ==========:|", "AmongUsClientOnPlayerJoindPatch");
+    }
+
+    // 参考 => https://g.co/gemini/share/a44da4e0b41a
+    /// <summary>既入室者の情報/警告メッセージを組み立てる</summary>
+    /// <param name="warningTextDic">Key =>警戒対象者のClient Id / Value => 警告テキストの辞書</param>
+    /// <returns>チャットに表示する情報/警告メッセージ</returns>
+    private static string BuildPlayerWarningMessage(in Dictionary<int, string> warningTextDic)
+    {
+        // 警告メッセージ用の変数を定義
+        string title;
+        string color;
+        string content;
+
+        // warningTextDicに警告があるかどうかで処理を分岐
+        if (warningTextDic.Any())
+        {
+            // 警告がある場合のタイトル、色、内容を設定
+            title = "警告!";
+            color = "#F2E700";
+            // string.Joinで効率的に文字列を結合し、最後に改行を追加
+            content = string.Join("\n", warningTextDic.Values) + "\n";
+        }
+        else
+        {
+            // 警告がない場合のタイトル、色、内容を設定
+            title = "Infomation";
+            color = "#89c3eb";
+            content = "現在, BANList対象者は入室しておりません。";
+        }
+
+        // 最後に組み立てた情報を使ってAddChatを一度だけ呼び出す
+        string chatMessage = $"<align=left><color={color}><size=150%>{title}</size></color><size=80%>\n{content}</size></align>";
+
+        return chatMessage;
     }
 }
